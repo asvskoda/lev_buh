a6b57ef4ac0f3fc83624cae238449d4eeba22351
a.vasilyev <skoda.alex23@gmail.com>
2024-05-08 17:22:16 +0300

    refactor: fix header and footer

--- frontend/modules/main/views/layouts/main.php
+++ frontend/modules/main/views/layouts/main.php
@@ -5,33 +5,45 @@
 
 use common\widgets\Alert;
 use frontend\modules\main\assets\AppAsset;
-use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
+use yii\bootstrap5\Breadcrumbs;
 
 $myAssetBundle = AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
     <!DOCTYPE html>
     <html lang='<?= Yii::$app->language ?>' class='h-100'>
+
     <head>
         <meta charset='<?= Yii::$app->charset ?>'>
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
         <?php $this->registerCsrfMetaTags() ?>
+        <?php
+        echo "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'>";
+        ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
+
     <body class='d-flex flex-column h-100'>
     <?php $this->beginBody() ?>
 
-    <header>
-        <div class='navigation-bar lev-font-color'>
-            <nav class='navbar navbar-expand-lg navbar-light fixed-top navbar-fixed-top'>
-                <div class='container'>
-                    <a class='navbar-brand d-md-block' href='/'>
-                        <?= Html::img($myAssetBundle->baseUrl . '/images/icon-round.png') ?>
-                    </a>
-                    <div class='collapse navbar-collapse' id='navbarMenuBlock'>
-                        <ul class='navbar-nav ml-auto'>
+    <header class='header'>
+        <div class='header-menu lev-font-color'>
+            <div class='container'>
+                <nav class='header-menu__container'>
+                    <div class='header-logo'>
+                        <a class='header-logo__link' href='/'>
+                            <?= Html::img($myAssetBundle->baseUrl . '/images/icon-round.png') ?>
+                        </a>
+                    </div>
+                    <div class='header-navbar'>
+                        <div class='header-navbar__btn'>
+                            <span class='header-navbar__btn-row'></span>
+                            <span class='header-navbar__btn-row'></span>
+                            <span class='header-navbar__btn-row'></span>
+                        </div>
+                        <ul class='header-navbar__list'>
                             <li class='nav-item'>
                                 <?= Html::a(Yii::t('app', 'Головна'), ['/'], ['class' => 'nav-link main']) ?>
                             </li>
@@ -45,88 +57,75 @@ $myAssetBundle = AppAsset::register($this);
                                 <?= Html::a(Yii::t('app', 'Контакти'), ['/contact'], ['class' => 'nav-link contact']) ?>
                             </li>
                         </ul>
+                        <a class='header-navbar__contacts' href='tel:+380986073304'>
+                            <span><?= Yii::t('app', 'Ми на зв\'язку') ?></span>
+                            <span>+38 (098) 607-33-04</span>
+                            <small>Пн.-Пт. з 09:00 до 18:00</small>
+                        </a>
                     </div>
-                    <div class='d-none d-sm-block'>
-                        <div class='text-center'>
-                            <small class='d-none d-md-block'><?= Yii::t('app', 'Зв\'язок') ?></br></small>
-                            <a class='text-decoration-none lev-font-color' href='tel:+380986073304'>098-607-33-04</br></a>
-                            <small class='d-none d-md-block'><small>Пн.-Пт. з 09:00 до 18:00</small></small>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-    </header>
-    <main role='main'>
-        <div>
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-                'class' => 'custom-breadcrumbs',
-            ]) ?>
-            <?= Alert::widget() ?>
-            <?= $content ?>
+                </nav>
+            </div>
         </div>
-    </main>
-    <hr class='before-footer-ln'>
 
-    <footer class='footer mt-auto py-3 lev-bg text-muted'>
-        <div class='container cw '>
-            <div class='row'>
-                <div class='col-lg-4'>
-                    <div class='row footer-menu-block'>
-                        <div class='col-lg-4 footer-brand'>
-                            <a class='navbar-brand d-md-block' href='/'>
-                                <?= Html::img($myAssetBundle->baseUrl . '/images/icon_60.png') ?>
-                                <?= Yii::t('app', 'Бухгалтерська агенція "ЛЕВ"') ?>
-                            </a>
-                        </div>
-                    </div>
+    </header>
+
+    <main role='main'>
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </main>
+
+    <footer class='footer lev-bg cw'>
+        <div class='container'>
+            <div class='footer-logo'>
+                <a class='cw td' href=' /'>
+                    <?= Html::img($myAssetBundle->baseUrl . '/images/icon-round.png') ?>
+                    <span><?= Yii::t('app', 'Бухгалтерська агенція "ЛЕВ"') ?></span>
+                </a>
+                <span class='f2'><?= Yii::t('app','Наш супровід - Ваш спокій') ?></span>
+            </div>
+
+            <div class='footer-contacts'>
+                <h2 class='fh2'><?= Yii::t('app','Контакти') ?></h2>
+                <a class='phone cw td' href='tel:+380986073304'>
+                    <i class='fa-solid fa-phone-volume'></i>
+                    +38 (098) 607-33-04
+                </a>
+                <a class='mail cw td' href='mailto:lev.buh.agency@gmail.com'>
+                    <i class='fa-solid fa-envelope'></i>
+                    lev.buh.agency@gmail.com
+                </a>
+                <div class='social-icons-block'>
+                    <a rel='noopener noreferrer' target='_blank' href='https://t.me/lev_buh' class='social-icons-link'>
+                        <i class='fa-brands fa-telegram cw'></i>
+                    </a>
+                    <a rel='noopener noreferrer' target='_blank' href='viber://chat?number=+380986073304' class='social-icons-link'>
+                        <i class='fa-brands fa-viber cw'></i>
+                    </a>
+                    <a rel='noopener noreferrer' target='_blank' href='https://www.instagram.com/lev_buh_agency' class='social-icons-link'>
+                        <i class='fa-brands fa-instagram cw'></i>
+                    </a>
+                    <a rel='noopener noreferrer' target='_blank' href='https://www.facebook.com/profile.php?id=61555635522199' class='social-icons-link'>
+                        <i class='fa-brands fa-facebook cw'></i>
+                    </a>
                 </div>
-                <div class='col-lg-4'>
-                    <p><?= Html::img($myAssetBundle->baseUrl . '/images/phone.png') ?></p>
-                    <p>
-                    <h2><?= Yii::t('app', 'Контакти') ?></h2></p>
-                    <p>+380986073304</p>
-                    <p>lev.buh.agency@gmail.com</p>
-                    <p>
-                    <ul class='social-icons-block'>
-                        <li class='social-icons icon-first'>
-                            <a rel='noopener noreferrer' target='_blank' href='https://t.me/lev_buh'
-                               class='social-icons-link'>
-                                <?= Html::img($myAssetBundle->baseUrl . '/images/telegram-48.png') ?>
-                            </a>
-                        </li>
-                        <li class='social-icons'>
-                            <a rel='noopener noreferrer' target='_blank' href='viber://chat?number=+380986073304'
-                               class='social-icons-link'>
-                                <?= Html::img($myAssetBundle->baseUrl . '/images/viber-48.png') ?>
-                            </a>
-                        </li>
-                        <li class='social-icons'>
-                            <a rel='noopener noreferrer' target='_blank' href='https://www.instagram.com/lev_buh_agency'
-                               class='social-icons-link'>
-                                <?= Html::img($myAssetBundle->baseUrl . '/images/insta-48.png') ?>
-                            </a>
-                        </li>
-                        <li class='social-icons'>
-                            <a rel='noopener noreferrer' target='_blank'
-                               href='https://www.facebook.com/profile.php?id=61555635522199' class='social-icons-link'>
-                                <?= Html::img($myAssetBundle->baseUrl . '/images/facebook-48.png') ?>
-                            </a>
-                        </li>
-                    </ul>
-                    </p>
-                </div>
-                <div class='col-lg-4'>
-                    <p><?= Html::img($myAssetBundle->baseUrl . '/images/map.png') ?></p>
-                    <p>
-                    <h2><?= Yii::t('app', 'Адреса офісу') ?></h2></p>
-                    <p><?= Yii::t('app', 'Харківська обл., смт Пісочин (с.Надточії), вул.Дачна 39') ?></p>
-                </div>
+            </div>
+
+            <div class='footer-address'>
+                <h2 class='fh2'><?= Yii::t('app','Адреса офісу') ?></h2>
+                <i class='fa-solid fa-location-dot cw'></i>
+                <?= Html::a(
+                        Yii::t('app', 'Харківська обл., смт Пісочин (с.Надточії), вул.Дачна 39'),
+                        ['/contact'],
+                        ['class' => 'td cw']
+                ) ?>
             </div>
         </div>
     </footer>
     <?php $this->endBody() ?>
     </body>
     </html>
+<?php $this->endPage() ?>
 
-<?php $this->endPage();
