165a9224998e6049fd3e9c32b3fef3e9624dd905
a.vasilyev <skoda.alex23@gmail.com>
2024-05-12 15:47:22 +0300

    refactor: serve and contact pages

--- frontend/modules/main/views/contact/index.php
+++ frontend/modules/main/views/contact/index.php
@@ -13,43 +13,37 @@ $this->title = 'Contact';
 $this->params['breadcrumbs'][] = $this->title;
 $myAssetBundle = AppAsset::register($this);
 ?>
-<div class='site-contact container'>
-    <div class='row'>
-        <div class='col-lg-5'>
-            <h2><?= Yii::t('app', 'Ми у соцмережах') ?></h2>
-            <ul class='social-icons-block'>
-                <li class='social-icons icon-first'>
-                    <a rel='noopener noreferrer' target='_blank' href='https://t.me/lev_buh'
-                       class='social-icons-link'>
-                        <?= Html::img($myAssetBundle->baseUrl . '/images/telegram-48.png') ?>
-                    </a>
-                </li>
-                <li class='social-icons'>
-                    <a rel='noopener noreferrer' target='_blank' href='viber://chat?number=+380986073304'
-                       class='social-icons-link'>
-                        <?= Html::img($myAssetBundle->baseUrl . '/images/viber-48.png') ?>
-                    </a>
-                </li>
-                <li class='social-icons'>
-                    <a rel='noopener noreferrer' target='_blank' href='https://www.instagram.com/lev_buh_agency'
-                       class='social-icons-link'>
-                        <?= Html::img($myAssetBundle->baseUrl . '/images/insta-48.png') ?>
-                    </a>
-                </li>
-                <li class='social-icons'>
-                    <a rel='noopener noreferrer' target='_blank'
-                       href='https://www.facebook.com/profile.php?id=61555635522199' class='social-icons-link'>
-                        <?= Html::img($myAssetBundle->baseUrl . '/images/facebook-48.png') ?>
-                    </a>
-                </li>
-            </ul>
+<div class='site-contact lev-font-color'>
+    <div class='contact-row'>
+        <h3>Контакти</h3>
+        <a class='phone lev-color td' href='tel:+380986073304'>
+            <i class='fa-solid fa-phone-volume'></i>
+            +38 (098) 607-33-04
+        </a>
+        <a class='mail lev-color td' href='mailto:lev.buh.agency@gmail.com'>
+            <i class='fa-solid fa-envelope'></i>
+            lev.buh.agency@gmail.com
+        </a>
+        <div class='social-icons-block'>
+            <a rel='noopener noreferrer' target='_blank' href='https://t.me/lev_buh' class='social-icons-link'>
+                <i class='fa-brands fa-telegram lev-color'></i>
+            </a>
+            <a rel='noopener noreferrer' target='_blank' href='viber://chat?number=+380986073304' class='social-icons-link'>
+                <i class='fa-brands fa-viber lev-color'></i>
+            </a>
+            <a rel='noopener noreferrer' target='_blank' href='https://www.instagram.com/lev.buh.agency' class='social-icons-link'>
+                <i class='fa-brands fa-instagram lev-color'></i>
+            </a>
+            <a rel='noopener noreferrer' target='_blank' href='https://www.facebook.com/profile.php?id=61557095614231' class='social-icons-link'>
+                <i class='fa-brands fa-facebook lev-color'></i>
+            </a>
         </div>
     </div>
-    <hr>
-    <div class='row'>
-        <div class='col-lg-5'>
+
+    <div class='contact-row'>
+        <div class="contact-column">
             <h3>
-                <?= Yii::t('app', 'Запис на консультацію') ?>
+                <?= Yii::t('app', 'Запис на консультацію:') ?>
             </h3>
             <?php $form = ActiveForm::begin(['id' => 'contact-form', 'action' => ['/main/home/create-consulting'], 'options' => []]); ?>
             <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
@@ -57,18 +51,18 @@ $myAssetBundle = AppAsset::register($this);
             <?= $form->field($model, 'phone') ?>
             <?= $form->field($model, 'question')->textInput() ?>
             <div class='form-group consulting-submit-button'>
-                <?= Html::submitButton(Yii::t('app', 'Надіслати'),
-                    ['class' => 'btn btn-success', 'name' => 'contact-button']) ?>
+                <?= Html::submitButton(
+                    Yii::t('app', 'Надіслати'),
+                    ['class' => 'btn btn-success lev-bg', 'name' => 'contact-button']
+                ) ?>
             </div>
             <?php ActiveForm::end(); ?>
         </div>
+        <div class="contact-column">
+            <h3>Адреса:</h3>
+            <p>Харківська обл., смт Пісочин (с.Надточії), вул.Дачна 39</p>
+            <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1283.0783815596037!2d36.07347663829013!3d49.97091863501656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41279fa6b9d47351%3A0x2fb47a934820d0d8!2z0JHRg9GF0LPQsNC70YLQtdGA0YHRjNC60LAg0LDQs9C10L3RhtGW0Y8gItCb0JXQkiI!5e0!3m2!1sru!2sua!4v1709484849995!5m2!1sru!2sua' width='600' height='350' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'>
+            </iframe>
+        </div>
     </div>
-    <hr>
-    <div class='row'>
-        <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1283.0783815596037!2d36.07347663829013!3d49.97091863501656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41279fa6b9d47351%3A0x2fb47a934820d0d8!2z0JHRg9GF0LPQsNC70YLQtdGA0YHRjNC60LAg0LDQs9C10L3RhtGW0Y8gItCb0JXQkiI!5e0!3m2!1sru!2sua!4v1709484849995!5m2!1sru!2sua'
-                width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy'
-                referrerpolicy='no-referrer-when-downgrade'>
-        </iframe>
-    </div>
-
 </div>
--- frontend/modules/main/views/layouts/main.php
+++ frontend/modules/main/views/layouts/main.php
@@ -88,7 +88,7 @@ $myAssetBundle = AppAsset::register($this);
             </div>
 
             <div class='footer-contacts'>
-                <h2 class='fh2'><?= Yii::t('app','Контакти') ?></h2>
+                <h2 class='footer-title'><?= Yii::t('app','Контакти') ?></h2>
                 <a class='phone cw td' href='tel:+380986073304'>
                     <i class='fa-solid fa-phone-volume'></i>
                     +38 (098) 607-33-04
@@ -104,22 +104,21 @@ $myAssetBundle = AppAsset::register($this);
                     <a rel='noopener noreferrer' target='_blank' href='viber://chat?number=+380986073304' class='social-icons-link'>
                         <i class='fa-brands fa-viber cw'></i>
                     </a>
-                    <a rel='noopener noreferrer' target='_blank' href='https://www.instagram.com/lev_buh_agency' class='social-icons-link'>
+                    <a rel='noopener noreferrer' target='_blank' href='https://www.instagram.com/lev.buh.agency' class='social-icons-link'>
                         <i class='fa-brands fa-instagram cw'></i>
                     </a>
-                    <a rel='noopener noreferrer' target='_blank' href='https://www.facebook.com/profile.php?id=61555635522199' class='social-icons-link'>
+                    <a rel='noopener noreferrer' target='_blank' href='https://www.facebook.com/profile.php?id=61557095614231' class='social-icons-link'>
                         <i class='fa-brands fa-facebook cw'></i>
                     </a>
                 </div>
             </div>
 
             <div class='footer-address'>
-                <h2 class='fh2'><?= Yii::t('app','Адреса офісу') ?></h2>
+                <h2 class='footer-title'><?= Yii::t('app','Адреса офісу') ?></h2>
                 <i class='fa-solid fa-location-dot cw'></i>
                 <?= Html::a(
                         Yii::t('app', 'Харківська обл., смт Пісочин (с.Надточії), вул.Дачна 39'),
-                        ['/contact'],
-                        ['class' => 'td cw']
+                        ['/contact']
                 ) ?>
             </div>
         </div>
