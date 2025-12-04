9406f23e3f31c5adfc79c6f6a276628f6fa0e148
a.vasilyev <skoda.alex23@gmail.com>
2025-12-04 04:16:32 +0300

    feature: added microsoft clarity tracking

--- frontend/modules/main/views/layouts/main.php
+++ frontend/modules/main/views/layouts/main.php
@@ -32,6 +32,15 @@ $currentParams = Yii::$app->request->getQueryParams();
         <?= "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'>" ?>
         <title><?= Html::encode(Yii::t('app', $this->title)) ?></title>
         <?php $this->head() ?>
+
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ug08ntfjuf");
+        </script>
+
     </head>
     <body>
     <?php $this->beginBody() ?>
